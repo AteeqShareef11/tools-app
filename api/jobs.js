bf8fd42ce6b92f8ec22f536f42729cea0ed3c98a
Muhammad Ateeq <36910001+AteeqShareef11@users.noreply.github.com>
2026-05-13 18:11:01 +0500

    job finder

--- api/jobs.js
+++ api/jobs.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
     const title = body.title;
     const skills = body.skills;
     const location = body.location;
+    const workType = body.workType;
 
     console.log("FIXED BODY:", body);
 
